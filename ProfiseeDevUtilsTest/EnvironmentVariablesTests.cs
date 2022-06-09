33ba972d0450316770c12093c089ffeadebae5cd
Patrick Mize <pmize3@gmail.com>
2022-06-09 17:21:14 -0400

    Add unit tests

---
+++ ProfiseeDevUtilsTest/EnvironmentVariablesTests.cs
@@ -0,0 +1,32 @@
+using NSubstitute;
+using NUnit.Framework;
+using ProfiseeDevUtils.Init;
+using ProfiseeDevUtilsTest.Mocks;
+using System;
+
+namespace ProfiseeDevUtilsTest
+{
+    public class EnvironmentVariablesTests
+    {
+        private EnvironmentVariablesMock environmentVariablesMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            environmentVariablesMock = new EnvironmentVariablesMock();
+        }
+
+        [Test]
+        public void EnvironmentVariables_Set_SetsEnvVars()
+        {            
+            this.environmentVariablesMock.Set();
+
+            var envVars = this.environmentVariablesMock.GetEnvironmentVariables();
+            foreach (var envVar in envVars)
+            {
+                var myEnvVar = Environment.GetEnvironmentVariable(envVar.Key, EnvironmentVariableTarget.User);
+                Assert.AreEqual(myEnvVar, envVar.Value, $"Expected {envVar.Key} to be {myEnvVar} but instead  was {envVar.Value}");
+            }
+        }
+    }
+}
---
+++ ProfiseeDevUtilsTest/Mocks/EnvironmentVariablesMock.cs
@@ -0,0 +1,20 @@
+﻿using ProfiseeDevUtils.Init;
+using System;
+using System.Collections.Generic;
+
+namespace ProfiseeDevUtilsTest.Mocks
+{
+    public class EnvironmentVariablesMock : EnvironmentVariables
+    {
+        private Dictionary<string, string> envVars = new Dictionary<string, string>();
+
+        public override void SetEnvironmentVariable(string variable, string value, EnvironmentVariableTarget envVarTarget)
+        {
+            this.envVars[variable] = value;
+        }
+        internal Dictionary<string, string> GetEnvironmentVariables()
+        {
+            return envVars;
+        }
+    }
+}
