f45a03d58e0b92479a1a2e0a494807ce2c874bf8
Raphael Miller <raphael.miller@profisee.com>
2022-06-09 13:17:00 -0400

    adding build folder

---
+++ Build/Utils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfiseeDevUtils.Build
+{
+    public class Utils
+    {
+
+        public void TurnOnService(string name)
+        {
+            ServiceController service = new ServiceController(name);
+            service.Start();
+            service.WaitForStatus(ServiceControllerStatus.Running);
+        }
+
+        public void TurnOffService(string name)
+        {
+
+        }
+
+        public void GetListOfAvailableProjects(string root)
+        {
+
+        }
+
+
+    }
+}
