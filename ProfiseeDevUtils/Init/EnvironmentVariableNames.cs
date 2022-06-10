e45ce2da849d2407790da5a244de017248e8a9bf
Patrick Mize <pmize3@gmail.com>
2022-06-10 10:11:31 -0400

    Make env var names consts

---
+++ ProfiseeDevUtils/Init/EnvironmentVariableNames.cs
@@ -0,0 +1,62 @@
+﻿namespace ProfiseeDevUtils.Init
+{
+    public class EnvironmentVariableNames
+    {
+        public const string AttachmentRepositoryLocation = "AttachmentRepositoryLocation";
+        public const string AttachmentRepositoryLogonType = "AttachmentRepositoryLogonType";
+        public const string AttachmentRepositoryUserName = "AttachmentRepositoryUserName";
+        public const string AttachmentRepositoryUserPassword = "AttachmentRepositoryUserPassword";
+        public const string MaestroWebAppName = "MaestroWebAppName";
+        public const string MaestroAppPoolName = "MaestroAppPoolName";
+        public const string MaestroAppPoolUserName = "MaestroAppPoolUserName";
+        public const string MaestroAppPoolUserPassword = "MaestroAppPoolUserPassword";
+        public const string MaestroDb = "MaestroDb";
+        public const string MaestroServicePort = "MaestroServicePort";
+        public const string MaestroServiceUserName = "MaestroServiceUserName";
+        public const string MaestroServiceUserPassword = "MaestroServiceUserPassword";
+        public const string MaestroVersion = "MaestroVersion";
+        public const string MsBuildUtil = "MsBuildUtil";
+        public const string MsTestUtil = "MsTestUtil";
+        public const string SetupTestCategory = "SetupTestCategory";
+        public const string SqlDataPath = "SqlDataPath";
+        public const string SqlServer = "SqlServer";
+        public const string SqlUserName = "SqlUserName";
+        public const string SqlUserPassword = "SqlUserPassword";
+        public const string TfsDrive = "TfsDrive";
+        public const string TfsGetSource = "TfsGetSource";
+        public const string TfsUtil = "TfsUtil";
+        public const string UseHttps = "UseHttps";
+        public const string UseSeparateDatabases = "UseSeparateDatabases";
+        public const string UseWindowsAuthentication = "UseWindowsAuthentication";
+        public const string WebSiteName = "WebSiteName";
+
+        // automation settings
+        public const string CrmDatabaseName = "CrmDatabaseName";
+        public const string CrmEventingDatabaseName = "CrmEventingDatabaseName";
+        public const string CrmSqlServer = "CrmSqlServer";
+        public const string PathToFederation = "PathToFederation";
+        public const string RunAsUserName = "RunAsUserName";
+        public const string ServerRESTUrl = "ServerRESTUrl";
+        public const string ServerRESTVersion = "ServerRESTVersion";
+        public const string ServerURL = "ServerURL";
+        public const string SqlEventingDatabaseName = "SqlEventingDatabaseName";
+        public const string SqlConnectorSqlServer = "SqlConnectorSqlServer";
+
+        // derived variables
+        public const string AutomationFolder = "AutomationFolder";
+        public const string BatchFileLocation = "BatchFileLocation";
+        public const string gitRepos = "gitRepos";
+        public const string LicenseFile = "LicenseFile";
+        public const string MaestroSnap = "MaestroSnap";
+        public const string MaestroSvc = "MaestroSvc";
+        public const string PathToUtilitiesExe = "PathToUtilitiesExe";
+        public const string ScriptsDrive = "ScriptsDrive";
+        public const string ServicesPublishPath = "ServicesPublishPath";
+        public const string TfsGetPlatformSource = "TfsGetPlatformSource";
+        public const string TfsProto = "TfsProto";
+        public const string TfsSdk = "TfsSdk";
+        public const string TfsSrc = "TfsSrc";
+        public const string UtilPath = "UtilPath";
+
+    }
+}
